2ceefdf4d4b348718aa3e543b6ce8dab1356b6dd
Jakkaphat <gunt@guntxjakka.me>
2022-04-30 19:21:17 +0700

    fix: add discord link back

---
+++ pages/dbot.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span
+      >redirecting you to
+      "https://discord.com/api/oauth2/authorize?client_id=893021470169632829&permissions=76800&scope=bot"</span
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  mounted() {
+    window.location =
+      'https://discord.com/api/oauth2/authorize?client_id=893021470169632829&permissions=76800&scope=bot' as any
+  },
+})
+</script>
